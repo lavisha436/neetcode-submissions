06554bc2c9090c04fefc4af567c202f74e958ec5
lavisha436 <lavishaarora436@gmail.com>
2026-06-22 00:00:28 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int cars[][] = new int [n][2];
+        for(int i = 0 ; i < n ; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars,(a,b) -> a[0]-b[0]);
+        int fleets = 0;
+        double fleetTime = 0;
+        for(int i = n-1 ; i >= 0 ; i--){
+            double currTime = (double)(target-cars[i][0])/cars[i][1];
+            if(currTime>fleetTime){
+                fleets++;
+                fleetTime = currTime;
+            }
+        }
+        return fleets;
+    }
+}
