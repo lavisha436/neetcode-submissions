e46d5854286a786f4dd05e197042ee12b8d6175e
lavisha436 <lavishaarora436@gmail.com>
2026-07-03 20:20:49 +0530

    Add: reverse-nodes-in-k-group - submission-1

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-1.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // if(head == null || k == 1){
+        //     return head;
+        // }
+        // ListNode dummy =  new ListNode(-1);
+        // dummy.next = head;
+        // ListNode groupPrev = dummy;
+        // while(true){
+        //     ListNode kth = groupPrev;
+        //     for(int i = 0 ; i < k ; i++){
+        //         kth = kth.next;
+        //         if(kth == null){
+        //             return dummy.next;
+        //         }
+        //     }
+        //     ListNode groupNext = kth.next;
+        //     ListNode prev = groupNext;
+        //     ListNode curr = groupPrev.next;
+
+        //     while(curr != groupNext){
+        //         ListNode next = curr.next;
+        //         curr.next = prev;
+        //         prev = curr;
+        //         curr = next;
+        //     }
+        //     ListNode groupStart = groupPrev.next;
+        //     groupPrev.next = kth;
+        //     groupPrev = groupStart;
+        // }
+        ListNode temp = head;
+        int count =0;
+        //check if k nodes exists
+        while(count < k){
+            if(temp == null){
+                return head;
+            }
+            temp = temp.next;
+            count++;
+        }
+        //recursively call for rest of the ll
+        ListNode prevNode = reverseKGroup(temp,k);
+
+        //reverse current node 
+        temp = head;
+        count = 0;
+        while(count < k){
+            ListNode next = temp.next;
+            temp.next = prevNode;
+            prevNode = temp;
+            temp = next;
+
+            count++;
+        }
+        return prevNode;
+    }     
+}
